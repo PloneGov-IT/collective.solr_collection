dd576930fdc1c3f9e29980b9f00b1263bb90147b
mamico <mauro@biodec.com>
2013-11-25 15:56:50 +0100

    index values

--- src/collective/solr_collection/browser/views.py
+++ src/collective/solr_collection/browser/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from archetypes.querywidget.views import MultiSelectWidget as BaseMultiSelectWidget
+# from archetypes.querywidget.views import sortable_value
+from collective.solr_collection.solr import solrUniqueValuesFor
+
 
 class MultiSelectWidget(BaseMultiSelectWidget):
 
@@ -8,6 +10,11 @@ class MultiSelectWidget(BaseMultiSelectWidget):
         if not useSolr:
             useSolr = self.request.form.get('useSolr', False)
         if useSolr:
-            # TODO: we must use solr if we get "user_solr" in the request
-            pass
+            # FIXME: could be better to override IQuerystringRegistryReader (?)
+            if not index:
+                index = self.request.form.get('index')
+            values = None
+            if index is not None:
+                values = solrUniqueValuesFor(index)
+            return dict((v, {'title': v}) for v in values)
         return BaseMultiSelectWidget.getValues(self, index=index)
